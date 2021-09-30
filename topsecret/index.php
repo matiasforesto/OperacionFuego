68c3cb0ec6e6c58218cc01d48a0142a0461e2aa9
Matias <matiasforesto07@gmail.com>
2021-09-30 02:38:50 +0000

    Creando arquitectura MVC

--- topsecret/index.php
+++ topsecret/index.php
@@ -1,28 +1,29 @@
 <?php
-//http://34.136.51.66/OperacionFuego/topsecret/
-//ini_set('display_errors','1');
-//require_once "../utils/utils.php";
+      //http://34.136.51.66/OperacionFuego/topsecret/
+      //ini_set('display_errors','1');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-  
-  if (isset($_POST['satellites'])){
 
-    require_once "../models/topSecret.php";
-    $satellites = $_POST['satellites']; //viene por el POST de topsecret
-    
-    $topSecret = new topSecret();
-    $position = $topSecret->topSecretIn($satellites);
+  if (isset($_POST['satellites']) ){
+      $satellites = $_POST['satellites']; //viene por el POST de topsecret
+  }
+  elseif(file_get_contents("php://input")){
+      $satellites = file_get_contents("php://input");// viene raw
+  }
+  else{
+      echo "No viene en el POST el JSON de satellites";
+      header("HTTP/1.1 400 Bad Request");
+      exit();
+  }
   
+   require_once "../controllers/topSecret.php";
+   $topSecretController = new topSecretController();
+   $position = $topSecretController->topSecretIn($satellites);
+   
     //respuesta
     header("HTTP/1.1 200 OK");
     print_r(json_encode($position));
-    
-  }else{
-      echo "No viene en el POST el JSON de satellites";
-      header("HTTP/1.1 400 Bad Request");
-  }
-  
-  exit();
+    exit();
 }
 
 //En caso de que ninguna de las opciones anteriores se haya ejecutado
